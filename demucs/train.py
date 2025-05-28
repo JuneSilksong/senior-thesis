76000589b30c941262b2fd368d41ceba2054222f
yellow-dash <shinobuki3770@gmail.com>
2025-05-29 01:58:46 +0900

    Update train.py

--- demucs/train.py
+++ demucs/train.py
@@ -3,7 +3,7 @@ np.float_ = np.float64
 import torch
 import torch.nn.functional as F
 from torch import optim
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 import matplotlib.pyplot as plt
 
 from demucs import Demucs
@@ -11,39 +11,39 @@ from musdb18 import MUSDB18, compare_sources
 
 def train():
     train_dataset = MUSDB18()
+    #train_dataset = Subset(train_dataset, [0])
     print(f"Dataset size: {len(train_dataset)}")
     train_loader = DataLoader(train_dataset, batch_size=4, shuffle=True, drop_last=True, num_workers=4, pin_memory=True)
-    # train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=4, pin_memory=True)
+    #train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False, num_workers=4, pin_memory=True)
 
     model = Demucs(sources=["vocals", "drums", "bass", "other"])
     model.cuda()
     model.train()
 
-    epochs = 2
+    batches = int(len(train_dataset)/train_loader.batch_size)
+
+    epochs = 5
     optimizer = optim.Adam(model.parameters(), lr=1e-4)
 
     for epoch in range(epochs):
-        n_track = 0
+        batch_i = 0
         for mix, sources in train_loader:
-            mix = mix.cuda() # (B, 2, T)
-            sources = sources.cuda() # (B, 4, 2, T)
+            mix = mix.cuda()
+            sources = sources.cuda()
 
-            B, S, C, T = sources.shape
+            est_sources = model(mix)
 
-            est_sources = model(mix) # (B, 8, T)
-            est_sources = est_sources.view(B, S, C, T) # (B, 4, 2, T)
-
-            loss = F.mse_loss(est_sources, sources)
+            loss = F.l1_loss(est_sources, sources)
 
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            n_track += 1
-            print(f"Epoch {epoch+1}/{epochs}, Track {n_track}, Loss = {loss.item():.4f}")
+            batch_i += 1
+            print(f"Epoch {epoch+1}/{epochs}, Batch {batch_i}/{batches}, Loss = {loss.item():.4f}")
         
-        print(f"Completed Epoch {epoch+1}/{epochs} with Loss = {loss.item():.4f}")
         compare_sources(sources, est_sources)
+        print(f"Completed Epoch {epoch+1}/{epochs} with Loss = {loss.item():.4f}")
 
     torch.save(model.state_dict(), 'demucs_checkpoint.pth')
 
